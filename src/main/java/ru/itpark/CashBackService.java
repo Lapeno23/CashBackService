078fbe5151a3c677f6bc63a6fa55eaee48792362
Lapeno23 <RVYakovenko@stud.kai.ru>
2018-11-21 23:31:54 +0300

    CashBack service v1.0

--- src/main/java/ru/itpark/CashBackService.java
+++ src/main/java/ru/itpark/CashBackService.java
@@ -1,43 +1,75 @@
 package ru.itpark;
 
 public class CashBackService {
+    private int regularPercent = 1;
     private int increasedPercent = 5;
     private int specialPercent = 30;
     private int maxCashBack = 3_000;
     private int regularPurchase;
     private int increasedPurchase;
     private int specialPurchase;
+    private int regularCashBack;
+    private int increasedCashBack;
+    private int specialCashBack;
 
-    private int RegularCashBack() {
-        return getRegularPurchase() * getRegularPercent() / 100;
-    }
-
-    private int increasedCashBack = increasedPurchase * increasedPercent / 100;
-    private int specialCashBack = specialPurchase * specialPercent / 100;
-
-    public int getMaxCashBack() {
-        return maxCashBack;
-    }
-
-    public int getRegularPercent() {
-        return 1;
-    }
-
-    public void setMaxCashBack(int maxCashBack) {
-        this.maxCashBack = maxCashBack;
-    }
-
-    public int getRegularPurchase() {
-        return regularPurchase;
-    }
 
     public void setRegularPurchase(int regularPurchase) {
         this.regularPurchase = regularPurchase;
     }
 
-    public void calculateCashBack(int regularPurchase) {
+    public void setIncreasedPurchase(int increasedPurchase) {
+        this.increasedPurchase = increasedPurchase;
+    }
+
+    public void setSpecialPurchase(int specialPurchase) {
+        this.specialPurchase = specialPurchase;
+    }
+
+    public void setRegularCashBack() {
+        this.regularCashBack = regularPurchase * regularPercent / 100;
+    }
+
+    public void setIncreasedCashBack() {
+        this.increasedCashBack = increasedPurchase * increasedPercent / 100;
+    }
+
+    public void setSpecialCashBack() {
+        this.specialCashBack = specialPurchase * specialPercent / 100;
+    }
+
+    public int getRegularCashBack() {
+        if (regularCashBack >= maxCashBack) {
+            return maxCashBack;
+        } else {
+            return regularCashBack;
+        }
+    }
+
+    public int getIncreasedCashBack() {
+        if (increasedCashBack >= maxCashBack) {
+            return maxCashBack;
+        } else {
+            return increasedCashBack;
+        }
+    }
+
+    public int getSpecialCashBack() {
+        if (specialCashBack >= maxCashBack) {
+            return maxCashBack;
+        } else {
+            return specialCashBack;
+        }
+    }
+
+    public void calculateCashBack(int regularPurchase, int increasedPurchase, int specialPurchase) {
         this.setRegularPurchase(regularPurchase);
-        CashBackService calculateRegularCashBack = new CashBackService();
-        int regularCashBack = calculateRegularCashBack.RegularCashBack();
+        this.setRegularCashBack();
+        this.setIncreasedPurchase(increasedPurchase);
+        this.setIncreasedCashBack();
+        this.setSpecialPurchase(specialPurchase);
+        this.setSpecialCashBack();
+        System.out.println(getRegularCashBack());
+        System.out.println(getIncreasedCashBack());
+        System.out.println((getSpecialCashBack()));
     }
 }
--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -3,6 +3,6 @@ package ru.itpark;
 public class Main {
     public static void main(String[] args) {
         CashBackService cashBackService = new CashBackService();
-        cashBackService.calculateCashBack(10_000);
+        cashBackService.calculateCashBack(1_000_000, 20_000, 100_000);
     }
 }
