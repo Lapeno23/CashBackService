7cac00217cde1e161e290bf53c1091b80f3baeb6
Lapeno23 <RVYakovenko@stud.kai.ru>
2018-11-28 17:14:30 +0300

    CashBackService v 1.4

---
+++ src/test/java/ru/itpark/CashBackServiceTest.java
@@ -0,0 +1,22 @@
+package ru.itpark;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashBackServiceTest {
+
+    @Test
+    void calculateCashBack() {
+        CashBackService cashBackService = new CashBackService();
+        assertEquals(3000, cashBackService.calculateCashBack(1_000_000,0,0));
+        assertEquals(1000, cashBackService.calculateCashBack(0,20_000,0));
+        assertEquals(3000, cashBackService.calculateCashBack(0,0,100_000));
+        assertEquals(100, cashBackService.calculateCashBack(10_000,0,0));
+        assertEquals(1500, cashBackService.calculateCashBack(0,0,5_000));
+        assertEquals(3000, cashBackService.calculateCashBack(0,200_000,0));
+
+
+
+    }
+}
